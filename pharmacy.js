6504bfdd22d7ba83e47a0e6044b533831e702f5a
David Hérault <dherault@gmail.com>
2019-02-28 12:09:36 +0100

    add Dafalgan and refactor pharmacy

--- pharmacy.js
+++ pharmacy.js
@@ -14,47 +14,45 @@ export class Pharmacy {
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
       const drug = this.drugs[i];
-      if (drug.name != "Herbal Tea" && drug.name != "Fervex") {
-        if (drug.benefit > 0) {
-          if (drug.name != "Magic Pill") {
-            drug.benefit = drug.benefit - 1;
-          }
-        }
-      } else {
+
+      // "Magic Pill" never expires nor decreases in Benefit.
+      if (drug.name === "Magic Pill") return this.drugs;
+
+      // Drugs that increase in benefit
+      if (drug.name === "Herbal Tea" || drug.name === "Fervex") {
         if (drug.benefit < 50) {
-          drug.benefit = drug.benefit + 1;
-          if (drug.name == "Fervex") {
-            if (drug.expiresIn < 11) {
-              if (drug.benefit < 50) {
-                drug.benefit = drug.benefit + 1;
-              }
+          drug.benefit++;
+          if (drug.name === "Fervex" && drug.benefit < 50) {
+            if (drug.expiresIn <= 10) {
+              drug.benefit++;
             }
-            if (drug.expiresIn < 6) {
-              if (drug.benefit < 50) {
-                drug.benefit = drug.benefit + 1;
-              }
+            if (drug.expiresIn <= 5) {
+              drug.benefit += 2;
             }
           }
         }
-      }
-      if (drug.name != "Magic Pill") {
-        drug.expiresIn = drug.expiresIn - 1;
-      }
-      if (drug.expiresIn < 0) {
-        if (drug.name != "Herbal Tea") {
-          if (drug.name != "Fervex") {
-            if (drug.benefit > 0) {
-              if (drug.name != "Magic Pill") {
-                drug.benefit = drug.benefit - 1;
-              }
-            }
+        // Drugs that decrease in benefit
+      } else {
+        if (drug.benefit > 0) {
+          if (drug.name === "Dafalgan") {
+            drug.benefit -= 2;
           } else {
-            drug.benefit = drug.benefit - drug.benefit;
+            drug.benefit--;
           }
-        } else {
+        }
+      }
+
+      drug.expiresIn--;
+
+      if (drug.expiresIn < 0) {
+        if (drug.name === "Herbal Tea") {
           if (drug.benefit < 50) {
-            drug.benefit = drug.benefit + 1;
+            drug.benefit++;
           }
+        } else if (drug.name === "Fervex") {
+          drug.benefit = 0;
+        } else if (drug.benefit > 0) {
+          drug.benefit--;
         }
       }
     }
