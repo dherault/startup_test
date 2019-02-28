d00218a5123914933ae348cd64d71b1d254af72f
David Hérault <dherault@gmail.com>
2019-02-28 12:53:27 +0100

    add tests

--- pharmacy.js
+++ pharmacy.js
@@ -1,8 +1,12 @@
+const minBenefit = 0;
+const maxBenefit = 50;
+
 export class Drug {
   constructor(name, expiresIn, benefit) {
     this.name = name;
     this.expiresIn = expiresIn;
-    this.benefit = benefit;
+    // NOTE: I did alter the Drug class to update how benefit is handled
+    this.benefit = Math.max(minBenefit, Math.min(benefit, maxBenefit));
   }
 }
 
@@ -16,42 +20,39 @@ export class Pharmacy {
       const drug = this.drugs[i];
 
       // "Magic Pill" never expires nor decreases in Benefit.
-      if (drug.name === "Magic Pill") return this.drugs;
+      if (drug.name === "Magic Pill") continue;
 
       // Drugs that increase in benefit
       if (drug.name === "Herbal Tea" || drug.name === "Fervex") {
-        if (drug.benefit < 50) {
-          drug.benefit++;
-          if (drug.name === "Fervex" && drug.benefit < 50) {
-            if (drug.expiresIn <= 10) {
-              drug.benefit++;
-            }
-            if (drug.expiresIn <= 5) {
-              drug.benefit += 2;
-            }
+        if (drug.name === "Fervex") {
+          if (drug.expiresIn <= 5) {
+            drug.benefit = Math.min(maxBenefit, drug.benefit + 3);
+          } else if (drug.expiresIn <= 10) {
+            drug.benefit = Math.min(maxBenefit, drug.benefit + 2);
+          } else {
+            drug.benefit = Math.min(maxBenefit, drug.benefit + 1);
           }
+        } else {
+          drug.benefit = Math.min(maxBenefit, drug.benefit + 1);
         }
         // Drugs that decrease in benefit
       } else {
-        if (drug.benefit > 0) {
-          if (drug.name === "Dafalgan") {
-            drug.benefit -= 2;
-          } else {
-            drug.benefit--;
-          }
+        if (drug.name === "Dafalgan") {
+          drug.benefit = Math.max(minBenefit, drug.benefit - 2);
+        } else {
+          drug.benefit = Math.max(minBenefit, drug.benefit - 1);
         }
       }
 
       drug.expiresIn--;
 
+      // "Herbal Tea" actually increases in Benefit the older it gets
+      if (drug.name === "Herbal Tea") continue;
+
       if (drug.expiresIn < 0) {
-        if (drug.name === "Herbal Tea") {
-          if (drug.benefit < 50) {
-            drug.benefit++;
-          }
-        } else if (drug.name === "Fervex") {
+        if (drug.name === "Fervex") {
           drug.benefit = 0;
-        } else if (drug.benefit > 0) {
+        } else if (drug.benefit > minBenefit) {
           drug.benefit--;
         }
       }
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,107 @@
 import { Drug, Pharmacy } from "./pharmacy";
+import assert from "assert";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
       [new Drug("test", 1, 2)]
     );
+
+    expect(new Pharmacy([new Drug("test", 1, 2)]).updateBenefitValue()).toEqual(
+      [new Drug("test", 0, 1)]
+    );
+  });
+
+  it("should once the expiration date has passed degrade benefit twice as fast", () => {
+    expect(new Pharmacy([new Drug("test", 0, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("test", -1, 1)]
+    );
+
+    expect(
+      new Pharmacy([new Drug("test", -1, 1)]).updateBenefitValue()
+    ).toEqual([new Drug("test", -2, 0)]);
+  });
+
+  it("should never have a negative benefit and a benefit over 50", () => {
+    const drugs = [];
+    const drugNames = [
+      "Doliprane",
+      "Herbal Tea",
+      "Fervex",
+      "Magic Pill",
+      "Dafalgan",
+      "test"
+    ];
+
+    drugNames.forEach(drugName => {
+      for (let expiresIn = -10; expiresIn <= 10; expiresIn++) {
+        for (let benefit = 0; benefit <= 60; benefit++) {
+          drugs.push(new Drug(drugName, expiresIn, benefit));
+        }
+      }
+    });
+
+    drugs.forEach(drug => {
+      const pharmacy = new Pharmacy([drug]);
+
+      for (let i = 0; i < 60; i++) {
+        const updatedDrug = pharmacy.updateBenefitValue()[0];
+
+        assert(updatedDrug.benefit >= 0);
+        assert(updatedDrug.benefit <= 50);
+      }
+    });
+  });
+
+  it("should increase Herbal Tea's benefit the older it gets", () => {
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", 1, 4)]);
+
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", -1, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", -2, 4)]);
+  });
+
+  it("should never expire of decrease in benefit Magic Pill", () => {
+    expect(
+      new Pharmacy([new Drug("Magic Pill", 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Magic Pill", 2, 3)]);
+  });
+
+  it("should increase Fervex's benefit appropriately ", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 20, 30)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 19, 31)]);
+
+    expect(
+      new Pharmacy([new Drug("Fervex", 10, 30)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 9, 32)]);
+
+    expect(
+      new Pharmacy([new Drug("Fervex", 6, 30)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 5, 32)]);
+
+    expect(
+      new Pharmacy([new Drug("Fervex", 5, 30)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 4, 33)]);
+
+    expect(
+      new Pharmacy([new Drug("Fervex", 4, 30)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 3, 33)]);
+
+    expect(
+      new Pharmacy([new Drug("Fervex", 0, 30)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", -1, 0)]);
+  });
+
+  it("should decrease Dafalgan's benefit twice as fast", () => {
+    expect(
+      new Pharmacy([new Drug("Dafalgan", 20, 20)]).updateBenefitValue()
+    ).toEqual([new Drug("Dafalgan", 19, 18)]);
+
+    expect(
+      new Pharmacy([new Drug("Dafalgan", 19, 18)]).updateBenefitValue()
+    ).toEqual([new Drug("Dafalgan", 18, 16)]);
   });
 });
